d3325411afa3cdaa25653a5d3ea460d98dd3f10c
Berkxp <berkxp1@gmail.com>
2025-07-13 14:35:47 -0300

    Create kali_install.sh

---
+++ install/kali_install.sh
@@ -0,0 +1,3 @@
+sudo apt install python git -y
+pip install colorama sockets argparse
+echo -e "\n[+] Instalação concluída com sucesso, para iniciar a ferramenta digite "python multidos.py lista.txt (porta)"\n"
