0132dcc7d8a48a0ad49db62994a66dbd991c28ef
Berkxp <berkxp1@gmail.com>
2025-07-13 14:26:43 -0300

    Add files via upload

--- install/termux_install.sh
+++ install/termux_install.sh
@@ -1,1 +1,5 @@
-
+apt install python -y
+apt install python-pip -y
+pip install colorama sockets argparse urlparser
+echo -e "\n[+] Instalação concluída com sucesso! para iniciar a ferramenta digite python multidos.py (ip) (porta) [timeout (opcional)]\n"
+cd ..
